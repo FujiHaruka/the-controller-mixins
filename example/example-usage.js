2a2a9b70482f35cf265c113d5d64e3ecd014e5d0
okunishinishi <okunishinishi@gmail.com>
2018-01-12 18:53:18 +0900

    Releasing new package

--- example/example-usage.js
+++ example/example-usage.js
@@ -4,7 +4,9 @@ const {TheCtrl} = require('the-controller-base')
 const {withAuthorized} = require('the-controller-mixins')
 
 async function tryExample () {
-
+  class MyCtrl extends withAuthorized(TheCtrl) {
+    /* ... */
+  }
 }
 
 tryExample().catch((err) => console.error(err))
